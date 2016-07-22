b326f6a5fb548949132cd041eed858da26e16fce
altrdgenetics <tofudeliveryae86@hotmail.com>
2016-07-22 15:58:03 -0400

    added in some javadoc style comments and changed out the timer so you can quit the application before sending the shutdown command

--- src/main/java/com/sceneControllers/MainWindowSceneController.java
+++ src/main/java/com/sceneControllers/MainWindowSceneController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -29,7 +30,7 @@ public class MainWindowSceneController implements Initializable {
 
     Stage mainstage;
     private double X, Y;
-    
+
     @FXML
     private TextField timeTextField;
     @FXML
@@ -44,67 +45,90 @@ public class MainWindowSceneController implements Initializable {
         X = mainstage.getX() - event.getScreenX();
         Y = mainstage.getY() - event.getScreenY();
     }
+
     @FXML
     protected void onRectangleDragged(MouseEvent event) {
         mainstage.setX(event.getScreenX() + X);
         mainstage.setY(event.getScreenY() + Y);
     }
-    
+
     /**
      * Initializes the controller class.
+     *
      * @param url
      * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //TODO
-    }    
-    
-    public void setDefaults(Stage stage){
+        //Numeric Field Value Only
+        timeTextField.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                timeTextField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+
+        //No Time Value
+        startButton.disableProperty().bind(timeTextField.textProperty().isEmpty());
+    }
+
+    /**
+     * Set Defaults for the controller used for passing variables during
+     * initialization.
+     *
+     * @param stage This is the FXML stage file reference.
+     */
+    public void setDefaults(Stage stage) {
         mainstage = stage;
     }
 
+    /**
+     * Exits the Application
+     */
     @FXML
     private void close() {
         System.exit(0);
     }
-    
+
+    /**
+     * Initiates the countdown timer.
+     */
     @FXML
     private void actionStartButton() {
         if (timeTextField.getText().trim().matches("\\d+")) {
-            try {
-                Runtime.getRuntime().exec(shutdownCommand());
-                progressBarThread();
-            } catch (IOException ex) {
-                Logger.getLogger(MainWindowSceneController.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            progressBarThread();
         } else {
             System.out.println("Entered Time Is Not Valid");
         }
     }
 
-    private String shutdownCommand(){
-        String timeInSeconds = String.valueOf(Integer.valueOf(timeTextField.getText().trim()) * 60);
-
+    /**
+     * Verifies the OS in use
+     *
+     * @return OS dependant shutdown string for command line execution.
+     */
+    private String shutdownCommand() {
         if (SystemUtils.IS_OS_WINDOWS) {
-            return "shutdown.exe -s -f -t " + timeInSeconds;
+            return "shutdown.exe -s -f -t";
         } else if (SystemUtils.IS_OS_AIX) {
-            return "shutdown -Fh " + timeInSeconds;
+            return "shutdown -Fh";
         } else if (SystemUtils.IS_OS_HP_UX) {
-            return "shutdown -hy " + timeInSeconds;
+            return "shutdown -hy";
         } else if (SystemUtils.IS_OS_IRIX) {
-            return "shutdown -y -g " + timeInSeconds;
+            return "shutdown -y -g";
         } else if (SystemUtils.IS_OS_SOLARIS || SystemUtils.IS_OS_SUN_OS) {
-            return "shutdown -y -i5 -g" + timeInSeconds;
-        } else if (SystemUtils.IS_OS_FREE_BSD || SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC 
-                || SystemUtils.IS_OS_MAC_OSX || SystemUtils.IS_OS_NET_BSD || SystemUtils.IS_OS_OPEN_BSD 
+            return "shutdown -y -i5 -g";
+        } else if (SystemUtils.IS_OS_FREE_BSD || SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC
+                || SystemUtils.IS_OS_MAC_OSX || SystemUtils.IS_OS_NET_BSD || SystemUtils.IS_OS_OPEN_BSD
                 || SystemUtils.IS_OS_UNIX) {
-            return "shutdown -h " + timeInSeconds;
+            return "shutdown -h ";
         } else {
             return "";
         }
     }
-    
+
+    /**
+     * Looped thread to update the progress bar
+     */
     private void progressBarThread() {
         Thread timerThread = new Thread() {
             @Override
@@ -121,15 +145,26 @@ public class MainWindowSceneController implements Initializable {
                     currentTime++;
                     updateProgressBar(currentTime, totalTime);
                 }
+                try {
+                    Runtime.getRuntime().exec(shutdownCommand());
+                } catch (IOException ex) {
+                    Logger.getLogger(MainWindowSceneController.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         };
         timerThread.start();
     }
-    
+
+    /**
+     *
+     * @param currentValue Current time value
+     * @param maxValue Total time value (Received from timer text field on this
+     * scene)
+     */
     private void updateProgressBar(final double currentValue, final double maxValue) {
         Platform.runLater(() -> {
             progressBar.setProgress(currentValue / maxValue);
         });
     }
-        
+
 }
